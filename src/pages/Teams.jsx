499f6bd02e04b03d9560d125b4c60953b15896af
Miguelpasitos98 <Miguelpasitos98@gmail.com>
2026-09-22 00:46:51 +0200

    Remove sidebar visibility effect in Teams component
    Removed sidebar visibility management from Teams component.

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1187,15 +1187,6 @@ const kitHomeUrl =
   );
 }
 export default function Teams() {
-  const { setSidebarVisible } = useOutletContext() || {};
-
-  useEffect(() => {
-    setSidebarVisible?.(false);
-
-    return () => {
-      setSidebarVisible?.(true);
-    };
-  }, [setSidebarVisible]);
 
   const [teams, setTeams] = useState([]);
   const [countries, setCountries] = useState([]);
